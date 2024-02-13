e1b31c14fa9636e3a5ffae6a243e6021818c59ed
Nikki Little <nikkilittle13@gmail.com>
2024-02-13 13:06:29 -0500

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyTag = technologies.map((technology, index) => <span key={index}>{technology}</span>)
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologyTag}
       </div>
     </div>
   );
